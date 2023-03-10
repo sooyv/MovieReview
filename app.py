bea20ea786fadf90526e1fd418d7e53d4d87427c
2cow0.0l@gmail.com <2cow0.0l@gmail.com>
2023-03-10 16:51:57 +0900

    Movie Review Application

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+import certifi
+ca = certifi.where()
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://test:sparta@cluster0.y0rajr2.mongodb.net/movies?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    og_image = soup.select_one('meta[property="og:image"]')
+    og_title = soup.select_one('meta[property="og:title"]')
+    og_description = soup.select_one('meta[property="og:description"]')
+
+    image = og_image['content']
+    title = og_title['content']
+    desc = og_description['content']
+
+    doc = {
+        'title':title,
+        'image':image,
+        'desc':desc,
+        'star':star_receive,
+        'comment':comment_receive
+    }
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg': '작성 완료!'})
+
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    movies_list = list(db.movies.find({},{'_id':False}))
+    return jsonify({'movies':movies_list})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
